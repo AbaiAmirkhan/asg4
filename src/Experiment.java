2fa44c4b4edd0842f7d6b40cebc851ebf971b53d
Abai Amirkhan <254410@astanait.edu.kz>
2026-05-10 17:10:03 +0500

    Update Experiment.java

--- src/Experiment.java
+++ src/Experiment.java
@@ -1,6 +1,6 @@
 public class Experiment {
 
-    // Run BFS and DFS with execution time measurement
+    
     public void runTraversals(Graph g) {
 
         long startBFS = System.nanoTime();
@@ -18,7 +18,7 @@ public class Experiment {
         System.out.println("DFS Execution Time: " + dfsTime + " ns");
     }
 
-    // Create and test graphs of different sizes
+    
     public void runMultipleTests() {
         int[] sizes = {10, 30, 100};
 
@@ -29,22 +29,20 @@ public class Experiment {
 
             Graph graph = new Graph();
 
-            // Add vertices
+            
             for (int i = 0; i < size; i++) {
                 graph.addVertex(new Vertex(i));
             }
 
-            // Add edges
+            
             for (int i = 0; i < size - 1; i++) {
                 graph.addEdge(i, i + 1);
 
-                // Additional edges for more connectivity
                 if (i + 2 < size) {
                     graph.addEdge(i, i + 2);
                 }
             }
 
-            // Print graph only for small graph
             if (size == 10) {
                 graph.printGraph();
             }
